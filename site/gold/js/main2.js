c28b55f941d2d9d097c62d78821ae7e7c3ff7a01
Tim Cook <redeemingdesign@gmail.com>
2012-10-25 18:54:36 -0500

    no message

---
+++ site/gold/js/main2.js
@@ -0,0 +1,27 @@
+
+
+$('tagcar').on('pageinit', function (){
+	var tagForm = $('#addItem');
+	tagForm.validate({
+		invalidHandler: function (form, validator) {
+			
+		},
+		submitHandler: function () {
+			var data = tagForm.serializeArray();
+			storeData(this.key);
+			window.location.reload();
+		}
+	});
+});
+
+var conditionValue,
+	holdValues
+;
+
+
+
+
+
+
+
+
