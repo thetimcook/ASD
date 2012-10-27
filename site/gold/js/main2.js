ece5691222f127c0f84b3ebd635470c188d930c4
Tim Cook <redeemingdesign@gmail.com>
2012-10-27 10:50:37 -0500

    saving to local storage

--- site/gold/js/main2.js
+++ site/gold/js/main2.js
@@ -14,21 +14,48 @@ $('#tagcar').on('pageinit', function (){
 	});
 });
 
-	var make = $('#make');
-		model = $('#model');
-		year = $('#year');
-		color = $('#color');
+var make = $('#make');
+	model = $('#model');
+	year = $('#year');
+	color = $('#color');
+	describe = $('#describe');
+		
+function storeData(key) {
+	if (!key) {
+		var id = $.now();
+	} else {
+		id = key;
+	}
+	
+	var condition = $('form input:checked').val();
+	var display = getCheckboxValue();
 
 	var car				= {};
 		car.make		= ["Make: ", make.val()];
 		car.model		= ["Model: ", model.val()];
 		car.year		= ["Year: ", year.val()];
 		car.color		= ["Color: ", color.val()];
+		car.condition	= ["Condition: ", condition];
+		car.display		= ["What makes it stand out? ", display];
+		car.describe	= ["Describe the car in your own words. ", describe.val()];
 		
-	color.focusout(function(){
-		console.log(color.val());
-	});
-	
+	localStorage.setItem(id, JSON.stringify(car));
+	alert("Car Tagged!");
+
+
+}
+
+function getCheckboxValue() {
+	var checkboxes = document.forms[0].display;
+	var holdValues = [];
+	for (var i=0, j=checkboxes.length; i<j; i++){
+		if(checkboxes[i].checked){
+			var checkedValue = checkboxes[i].value;
+			holdValues.push(checkedValue);
+		}
+	}
+	return holdValues;	
+}
 
 
 
