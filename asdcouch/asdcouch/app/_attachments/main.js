94b9dd418d0871e0b34e90e8484d7925e87ac2a9
Tim Cook <redeemingdesign@gmail.com>
2012-12-06 18:05:29 -0600

    cloud ant and couched working with separate files, not app

---
+++ asdcouch/asdcouch/app/_attachments/main.js
@@ -0,0 +1,37 @@
+$(document).ready(function(){
+	$.ajax({
+		url: "_view/cars",
+		dataType: "json",
+		success: function(data) {
+			$.each(data.rows, function(index, cars) {
+				var key = cars.key
+					make = cars.value.make,
+					model = cars.value.model,
+					year = cars.value.year,
+					color = cars.value.color,
+					display = cars.value.display,
+					condition = cars.value.condition,
+					describe = cars.value.describe
+				$(''+
+					'<div data-role="collapsible">'+
+						'<h3>'+ key +'</h3>'+
+						'<p>Make: '+ make +'</p>'+
+						'<p>Model: '+ model +'</p>'+
+						'<p>Year: '+ year +'</p>'+
+						'<p>Color: '+ color +'</p>'+
+						'<p>What makes it stand out? '+ display +'</p>'+
+						'<p>Condition: '+ condition +'</p>'+
+						'<p>Describe the car in your own words. '+ describe +'</p>'+
+					'</div>'
+				).appendTo('#carlist');
+			});
+		$('#carlist').trigger("create");
+		}
+	});
+});
+
+
+
+
+
+
---
+++ asdcouch/asdcouch/app/views/cars/map.js
@@ -0,0 +1,13 @@
+function (doc) {
+	if (doc._id.substr(0, 3) === "car"){
+		emit(doc.make, {
+			"make": doc.make,
+			"model": doc.model,
+			"year": doc.year,
+			"color": doc.color,
+			"display": doc.display,
+			"condition": doc.condition,
+			"describe": doc.describe
+		});
+	}
+};
